7472a749020fa2515248b21da06a769814ac3529
gabithume <gabithume@gmail.com>
2015-11-17 17:46:11 -0200

    WIP: animating plot

--- visualization/plot.py
+++ visualization/plot.py
@@ -15,96 +15,124 @@ from sklearn.lda import LDA
 from matplotlib import pyplot as plt
 
 from sklearn.decomposition import PCA as sklearnPCA
+from bokeh.palettes import brewer
+from scipy.interpolate import interp1d
 
 features_file = sys.argv[1]
 images_dir = sys.argv[2]
 
 output_server("animated")
 # output_file("vis.html");
-p = figure(plot_width=600, plot_height=600)
+image_size = 300
+square_size = 10
+alpha = 0.9
 
 df = pd.io.parsers.read_csv(filepath_or_buffer = features_file, header=None, sep = ' ')
 df.dropna(how="all", inplace=True, axis=1)
 df.columns = ['Image label'] + ['Class label'] + ['Train or test'] + [l for l in range(3, df.shape[1])]
-X = df[range(3, df.shape[1])].values
-y = df['Class label'].values
+all_samples = df[range(3, df.shape[1])].values
+classes = df['Class label'].values
 
-num_classes = len(np.unique(y))
+sklearn_pca = sklearnPCA(n_components=2)
+X_pca = sklearn_pca.fit_transform(all_samples)
+p = figure(title = "artificial generation", x_range=[min(X_pca[:,0]), max(X_pca[:,0])], y_range=[min(X_pca[:,1]), max(X_pca[:,1])])
+
+num_classes = len(np.unique(classes))
 label_dict = {0: 'Elefante', 1: 'Cavalo'}
 
 trainOrTest = df['Train or test'].values
 treino = trainOrTest == 1
 teste = trainOrTest == 2
 
-generated = (y==1)*treino
+generated = (classes==1)*treino
 generated[100] = False
-original = ~generated
+original = all_samples
 
-classes = []
-for i in range(2):
-    classes.append({'treino': (y==i)*treino*~generated, 'teste': (y==i)*teste*~generated})
+classes_samples = []
+for i in range(num_classes):
+    classes_samples.append({'treino': (classes==i)*treino*~generated, 'teste': (classes==i)*teste*~generated})
 
-image_size = 300
-square_size = 10
-alpha = 0.9
+if num_classes < 3:
+    colors = brewer["Spectral"][3]
+else:
+    colors = brewer["Spectral"][num_classes]
+
+samples = []
+for label in range(num_classes):
+    for status in ('treino', 'teste'):
+        samples.append([(classes_samples[label][status])[~generated],
+                        colors[label],
+                        label_dict[label]+" - "+status,
+                        status+str(label)])
 
 # def animated_generation(features):
 
 sklearn_pca = sklearnPCA(n_components=2)
-X = sklearn_pca.fit_transform(X)
-x_plot = X[:,0]
-y_plot = X[:,1]
+X_pca = sklearn_pca.fit_transform(original[~generated])
+x_plot = X_pca[:,0]
+y_plot = X_pca[:,1]
 
-for label,marker,color in zip(range(2),('^', 'o'),('blue', 'red')):
-    if label == 1:
-        # p.square(list(x_plot), list(y_plot), color = "Blue", legend= label_dict[label], size=square_size)
-        images = [images_dir+str(label)+"/treino/"+str(i)+".png" for i in range(0,6)]
-        p.square(x = list(x_plot[classes[label]['treino']]), y = list(y_plot[classes[label]['treino']]), color = "Red", legend= label_dict[label]+" - treino", size=square_size, line_dash = [6, 3], name = "treino"+str(label))
-        # p.image_url(x = list(x_plot[treino[from_class]]-image_size/2), y = list(y_plot[treino[from_class]]+image_size/2), url=images, global_alpha = alpha, h = image_size, w = image_size)
+samples.append([[], colors[num_classes], "Artificiais", "Generated"])
 
-        images = [images_dir+str(label)+"/teste/"+str(i)+".png" for i in range(50)]
-        p.square(x = list(x_plot[classes[label]['teste']]), y = list(y_plot[classes[label]['teste']]), color = "Yellow", legend= label_dict[label]+" - teste", size=square_size, line_dash = [6, 3], name = "teste"+str(label))
-        # p.image_url(x = list(x_plot[teste[from_class]]-image_size/2), y = list(y_plot[teste[from_class]]+image_size/2), url=images, global_alpha = alpha, h = image_size, w = image_size)
-    else:
-        images = [images_dir+str(label)+"/treino/"+str(i)+".png" for i in range(50)]
-        p.square(x = list(x_plot[classes[label]['treino']]), y = list(y_plot[classes[label]['treino']]), color = "Purple", legend=label_dict[label], size=square_size, line_dash = [6, 3], name = "treino"+str(label))
-        images = images + [images_dir+str(label)+"/teste/"+str(i)+".png" for i in range(50)]
-        p.square(x = list(x_plot[classes[label]['teste']]), y = list(y_plot[classes[label]['teste']]), color = "Purple", legend=label_dict[label], size=square_size, line_dash = [6, 3], name = "teste"+str(label))
-        # p.image_url(x = list(x_plot-image_size/2), y = list(y_plot+image_size/2), url=images, global_alpha = alpha, h = image_size, w = image_size)
+for index, color, label, name in samples:
+    p.square(x = list(x_plot[index]), y = list(y_plot[index]), color = color, legend = label, size = square_size, line_dash = [6, 3], name = name)
+
 show(p)
 
+# images = [images_dir+str(label)+"/treino/"+str(i)+".png" for i in range(0,6)]
+# p.image_url(x = list(x_plot[treino[from_class]]-image_size/2), y = list(y_plot[treino[from_class]]+image_size/2), url=images, global_alpha = alpha, h = image_size, w = image_size)
 
-for image in range(len(generated)):
+
+def animate (x0, y0, x1, y1, step):
+    x = x0*(x0<x1) + x1*(x1<x0) + abs(x1-x0)/step
+    y = y0 + (y1-y0)*((x-x0)/(x1-x0))
+    return x, y
+
+samples[-1][0] = len(X_pca)
+original = original[~generated]
+for image in generated.nonzero()[0]:
     sklearn_pca = sklearnPCA(n_components=2)
-    original = np.append(X, [X[generated[image]]], axis = 0)
+    original = np.append(original, [all_samples[image]], axis = 0)
     X_pca = sklearn_pca.fit_transform(original)
 
-    data = p.select(name = "treino1")[0].data_source
-    data.data['x'] = list(X_pca[:,0][classes[0]['treino']])
-    data.data['y'] = list(X_pca[:,1][classes[0]['treino']])
-    cursession().store_objects(data)
+    for step in range (10, 2, -2):
+        ds = []
+        for index, color, label, name in samples:
+            data = p.select(name = name)[0].data_source
+            x0 = data.data['x']
+            y0 = data.data['y']
+            if name is "Generated":
+                x1 = X_pca[:,0][index :]
+                y1 = X_pca[:,1][index :]
 
-    data = p.select(name = "teste1")[0].data_source
-    data.data['x'] = list(X_pca[:,0][classes[1]['teste']])
-    data.data['y'] = list(X_pca[:,1][classes[1]['teste']])
-    cursession().store_objects(data)
+                if len(x1)!=len(x0):
+                    x1 = x1[:len(x1)-1]
+                    y1 = y1[:len(y1)-1]
 
-    data = p.select(name = "treino0")[0].data_source
-    data.data['x'] = list(X_pca[:,0][classes[0]['treino']])
-    data.data['y'] = list(X_pca[:,1][classes[0]['treino']])
-    cursession().store_objects(data)
+            else:
+                x1 = X_pca[:,0][index]
+                y1 = X_pca[:,1][index]
 
-    data = p.select(name = "teste0")[0].data_source
-    data.data['x'] = list(X_pca[:,0][classes[0]['teste']])
-    data.data['y'] = list(X_pca[:,1][classes[0]['teste']])
-    cursession().store_objects(data)
+            x, y = x0, y0
+            x, y = animate (x, y, x1, y1, step)
+            data.data['x'] = list(x)
+            data.data['y'] = list(y)
+            ds.append(data)
+
+            if name is "Generated":
+                data.data['x'] = list(X_pca[:,0][index :])
+                data.data['y'] = list(X_pca[:,1][index :])
+            else:
+                data.data['x'] = list(x1)
+                data.data['y'] = list(y1)
+
+            ds.append(data)
+        cursession().store_objects(*ds)
 
     # img_file = [images_dir+str(label)+"/treino/"+str(image)+".png" ]
     # print img_file
-    # p.square(X_pca[:,0][-1], X_pca[:,1][-1], color = "Black", legend= " - artificiais", size=square_size, line_dash = [6, 3], name = "generated"+str(image))
     # p.image_url(x = X_pca[:,0][-1]-image_size/2, y = X_pca[:,1][-1]+image_size/2, url=img_file, global_alpha = alpha, h = image_size, w = image_size)
     #
-    # cursession().store_objects(p)
     # time.sleep(.10)
 
 # sklearn_lda = LDA(n_components=2)
