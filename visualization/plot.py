cfecc5bb1eb789dc290cc5e5aced13b8f57f2f6d
gabithume <gabithume@gmail.com>
2015-11-19 17:45:17 -0200

    WIP: attemp to show a dynamic decision region

--- visualization/plot.py
+++ visualization/plot.py
@@ -8,6 +8,7 @@ import time
 import sys
 
 from bokeh.plotting import output_file, figure, show, output_server, cursession
+from bokeh.palettes import brewer
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.lda import LDA
@@ -15,8 +16,35 @@ from sklearn.decomposition import PCA as sklearnPCA
 from sklearn.neighbors import KNeighborsClassifier
 
 from matplotlib import pyplot as plt
-from bokeh.palettes import brewer
 from scipy.interpolate import interp1d
+
+def pca(samples):
+    sklearn_pca = sklearnPCA(n_components=2)
+    X_pca = sklearn_pca.fit_transform(samples)
+
+    return (X_pca[:,0], X_pca[:,1])
+
+def decision_region((x, y), classes, plot_size):
+    # Plotting the decision region
+    h = abs(x.max() - x.min())/100
+    classifier = KNeighborsClassifier(n_neighbors = 1)
+    classifier.fit( np.concatenate(([x], [y]), axis = 0).T, classes)
+    if len(plot_size) != 4:
+        minx = x.min()-1
+        maxx = x.max()+1
+        miny = y.min()-1
+        maxy = y.max()+1
+    else:
+        minx, maxx, miny, maxy = plot_size
+    xx, yy = np.meshgrid(np.arange(minx, maxx, h), np.arange(miny, maxy, h))
+    region = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    return region.reshape(xx.shape)
+
+def between_point(x0, y0, x1, y1, step):
+    x = x0*(x0<x1) + x1*(x1<x0) + abs(x1-x0)/step
+    y = y0 + (y1-y0)*((x-x0)/(x1-x0))
+    return x, y
+
 
 features_file = sys.argv[1]
 images_dir = sys.argv[2]
@@ -31,50 +59,35 @@ df = pd.io.parsers.read_csv(filepath_or_buffer = features_file, header=None, sep
 df.dropna(how="all", inplace=True, axis=1)
 df.columns = ['Image label'] + ['Class label'] + ['Train or test'] + [l for l in range(3, df.shape[1])]
 all_samples = df[range(3, df.shape[1])].values
-classes = df['Class label'].values
-
-sklearn_pca = sklearnPCA(n_components=2)
-X_pca = sklearn_pca.fit_transform(all_samples)
-p = figure(title = "artificial generation", x_range=[min(X_pca[:,0]), max(X_pca[:,0])], y_range=[min(X_pca[:,1]), max(X_pca[:,1])])
-
-# Plotting the decision region
-classifier = KNeighborsClassifier(n_neighbors=1)
-classifier.fit(X_pca, classes)
-
-h = 1000
-x_min, x_max = X_pca[:, 0].min() - 1, X_pca[:, 0].max() + 1
-y_min, y_max = X_pca[:, 1].min() - 1, X_pca[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                     np.arange(y_min, y_max, h))
-print len(np.arange(x_min, x_max, h))
-
-region = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
-region = region.reshape(xx.shape)
-
-print region
-
-p.image(image=[region], x=[0], y=[0], dw=[1000], dh=[1000], palette="Spectral11")
-
-
-num_classes = len(np.unique(classes))
+all_classes = df['Class label'].values
+num_classes = len(np.unique(all_classes))
 label_dict = {0: 'Elefante', 1: 'Cavalo'}
 
 trainOrTest = df['Train or test'].values
 treino = trainOrTest == 1
 teste = trainOrTest == 2
 
-generated = (classes==1)*treino
+generated = (all_classes==1)*treino # todo generated set
 generated[100] = False
-original = all_samples
-
-classes_samples = []
-for i in range(num_classes):
-    classes_samples.append({'treino': (classes==i)*treino*~generated, 'teste': (classes==i)*teste*~generated})
+# Take only the original training samples
+original = all_samples[~generated]# * treino]
 
 if num_classes < 3:
     colors = brewer["Spectral"][3]
 else:
     colors = brewer["Spectral"][num_classes]
+
+x, y = pca(all_samples)
+p = figure(title = "Geracao Artificial de Imagens", x_range=[min(x), max(x)], y_range=[min(y), max(y)])
+
+region = decision_region((x, y), all_classes, ())
+p.image(image=[region], x=[min(x)], y=[min(y)], dw=[abs(max(x)-min(x))], dh=[abs(max(y)-min(y))], palette="Greys3", alpha = 0.1, name = "region")
+plot_size = region.shape
+
+classes_samples = []
+for i in range(num_classes):
+    classes_samples.append({'treino': (all_classes==i)*treino,
+                            'teste': (all_classes==i)*teste})
 
 samples = []
 for label in range(num_classes):
@@ -84,80 +97,63 @@ for label in range(num_classes):
                         label_dict[label]+" - "+status,
                         status+str(label)])
 
-# def animated_generation(features):
-
-sklearn_pca = sklearnPCA(n_components=2)
-X_pca = sklearn_pca.fit_transform(original[~generated])
-x_plot = X_pca[:,0]
-y_plot = X_pca[:,1]
-
 samples.append([[], colors[num_classes], "Artificiais", "Generated"])
-
 for index, color, label, name in samples:
-    p.square(x = list(x_plot[index]), y = list(y_plot[index]), color = color, legend = label, size = square_size, line_dash = [6, 3], name = name)
+    p.square(x = list(x[index]), y = list(y[index]), color = color,
+            legend = label, size = square_size, line_dash = [6, 3], name = name)
 
 show(p)
 
 # images = [images_dir+str(label)+"/treino/"+str(i)+".png" for i in range(0,6)]
 # p.image_url(x = list(x_plot[treino[from_class]]-image_size/2), y = list(y_plot[treino[from_class]]+image_size/2), url=images, global_alpha = alpha, h = image_size, w = image_size)
 
-
-def animate (x0, y0, x1, y1, step):
-    x = x0*(x0<x1) + x1*(x1<x0) + abs(x1-x0)/step
-    y = y0 + (y1-y0)*((x-x0)/(x1-x0))
-    return x, y
-
-samples[-1][0] = len(X_pca)
-original = original[~generated]
+samples[-1][0] = len(original)
+classes = all_classes[~generated]
 for image in generated.nonzero()[0]:
-    sklearn_pca = sklearnPCA(n_components=2)
     original = np.append(original, [all_samples[image]], axis = 0)
-    X_pca = sklearn_pca.fit_transform(original)
+    classes = np.append(classes, [all_classes[image]], axis = 0)
+    x_plot, y_plot = pca(original)
+    data = p.select(name = "region")[0].data_source
+    region = decision_region((x_plot, y_plot), classes, plot_size)
+    print region.shape
+    # data.data['image'] =
+    # cursession().store_objects(data)
 
-    for step in range (10, 2, -2):
-        ds = []
-        for index, color, label, name in samples:
-            data = p.select(name = name)[0].data_source
-            x0 = data.data['x']
-            y0 = data.data['y']
-            if name is "Generated":
-                x1 = X_pca[:,0][index :]
-                y1 = X_pca[:,1][index :]
+    ds = []
+    for index, color, label, name in samples:
+        data = p.select(name = name)[0].data_source
+        x0 = data.data['x']
+        y0 = data.data['y']
+        if name is "Generated":
+            x1 = x_plot[index :]
+            y1 = y_plot[index :]
 
-                if len(x1)!=len(x0):
-                    x1 = x1[:len(x1)-1]
-                    y1 = y1[:len(y1)-1]
+            if len(x1) != len(x0):
+                x1 = x1[:len(x1)-1]
+                y1 = y1[:len(y1)-1]
 
-            else:
-                x1 = X_pca[:,0][index]
-                y1 = X_pca[:,1][index]
+        else:
+            x1 = x_plot[index]
+            y1 = y_plot[index]
 
-            x, y = x0, y0
-            x, y = animate (x, y, x1, y1, step)
-            data.data['x'] = list(x)
-            data.data['y'] = list(y)
-            ds.append(data)
+        x, y = x0, y0
+        x, y = between_point (x, y, x1, y1, 2)
+        data.data['x'] = list(x)
+        data.data['y'] = list(y)
+        ds.append(data)
 
-            if name is "Generated":
-                data.data['x'] = list(X_pca[:,0][index :])
-                data.data['y'] = list(X_pca[:,1][index :])
-            else:
-                data.data['x'] = list(x1)
-                data.data['y'] = list(y1)
+        if name is "Generated":
+            data.data['x'] = list(x_plot[index :])
+            data.data['y'] = list(y_plot[index :])
+        else:
+            data.data['x'] = list(x1)
+            data.data['y'] = list(y1)
 
-            ds.append(data)
-        cursession().store_objects(*ds)
+        ds.append(data)
+    cursession().store_objects(*ds)
 
     # img_file = [images_dir+str(label)+"/treino/"+str(image)+".png" ]
     # print img_file
     # p.image_url(x = X_pca[:,0][-1]-image_size/2, y = X_pca[:,1][-1]+image_size/2, url=img_file, global_alpha = alpha, h = image_size, w = image_size)
     #
     # time.sleep(.10)
-
-# sklearn_lda = LDA(n_components=2)
-# X_lda = sklearn_lda.fit_transform(X,y)
-#
-# plot_scikit(X_lda, 'LDA')
-# show(p)
-
-# animated_generation(X)
