0e3fc14cd7ecdd1f647ff4d95fc9b91bf1e93c03
gabithume <gabithume@gmail.com>
2015-11-25 18:18:51 -0200

    WIP: perform pca from svd

--- visualization/plot.py
+++ visualization/plot.py
@@ -8,14 +8,14 @@ import time
 import sys
 
 from bokeh.io import hplot, vform
-from bokeh.plotting import figure, show, output_server, cursession
+from bokeh.plotting import figure, show, output_server, cursession, gridplot, output_file
 from bokeh.palettes import brewer
-from bokeh.models.widgets import Button
+from bokeh.models.widgets import Button, Tabs, Panel
 from bokeh.properties import Instance
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.lda import LDA
-from sklearn.decomposition import PCA as sklearnPCA
+from sklearn.decomposition import PCA as sklearnPCA, TruncatedSVD
 from sklearn.neighbors import KNeighborsClassifier
 
 from matplotlib import pyplot as plt
@@ -35,9 +35,12 @@ class plot(object):
         self.all_samples = df[range(4, df.shape[1])].values
         self.all_classes = df['Class label'].values
         self.num_classes = len(np.unique(self.all_classes))
+        self.images = df['Image path'].values
 
         trainOrTest = df['Train or test'].values
         self.treino = trainOrTest == 1
+        if self.treino.sum() == 0:
+            self.treino = trainOrTest == 0
         self.teste = trainOrTest == 2
 
         self.generated = df['Generated'].values == True
@@ -46,7 +49,7 @@ class plot(object):
         self.original = ~self.generated * self.treino
 
         if self.num_classes < 3:
-            self.colors = brewer["Spectral"][3]
+            self.colors = brewer["Spectral"][4]
         else:
             self.colors = brewer["Spectral"][self.num_classes]
 
@@ -79,10 +82,15 @@ class plot(object):
         '''
         Apply pca from sklearn and return the two most important dimensions.
         '''
-        sklearn_pca = sklearnPCA(n_components=2)
-        X_pca = sklearn_pca.fit_transform(samples)
-
-        return X_pca
+        # sklearn_pca = sklearnPCA(n_components=2)
+        # # TruncatedSVD, whiten
+        # X_pca = sklearn_pca.fit_transform(samples)
+        #
+        samples_mean = samples.mean(0)
+        samples_center = samples - samples_mean
+        eigenvectors, eigenvalues, vt = np.linalg.svd(samples_center)
+        dataReduced = np.dot(np.transpose(eigenvectors), samples_center)
+        return dataReduced
 
     def decision_region(self, features, classes):
         '''
@@ -118,6 +126,7 @@ class plot(object):
         current_samples = self.all_samples[self.original]
         curent_classes = self.all_classes[self.original]
         index_generation = len(current_samples)
+        new_data = []
         for image in self.generated.nonzero()[0]:
             current_samples = np.append(current_samples, [self.all_samples[image]], axis = 0)
             curent_classes = np.append(curent_classes, [self.all_classes[image]], axis = 0)
@@ -125,11 +134,10 @@ class plot(object):
             ds = []
             pca = self.pca(current_samples)
             x_plot, y_plot = (pca[:,0], pca[:,1])
-            data = p.select(name = "region")[0].data_source
-            region = self.decision_region(pca, curent_classes)
-            data.data['image'] = [region]
-            ds.append(data)
-            # cursession().store_objects(data)
+            # data = p.select(name = "region")[0].data_source
+            # region = self.decision_region(pca, curent_classes)
+            # data.data['image'] = [region]
+            # ds.append(data)
 
             for index, color, label, name, status in self.samples:
                 if p.select(name = name) != []:
@@ -138,7 +146,6 @@ class plot(object):
                     y0 = data.data['y']
 
                     if name is "Generated":
-                        print index
                         x1 = x_plot[index_generation :]
                         y1 = y_plot[index_generation :]
 
@@ -164,13 +171,12 @@ class plot(object):
                         data.data['y'] = list(y1)
 
                     ds.append(data)
-            cursession().store_objects(*ds)
-            # img_file = [images_dir+str(label)+"/treino/"+str(image)+".png" ]
-            # print img_file
-            # p.image_url(x = X_pca[:,0][-1]-image_size/2, y = X_pca[:,1][-1]+image_size/2, url=img_file, global_alpha = alpha, h = image_size, w = image_size)
-            #
-            # time.sleep(.10)
-
+            new_data.append(ds)
+        return new_data
+        # img_file = [images_dir+str(label)+"/treino/"+str(image)+".png" ]
+        # print img_file
+        # p.image_url(x = X_pca[:,0][-1]-image_size/2, y = X_pca[:,1][-1]+image_size/2, url=img_file, global_alpha = alpha, h = image_size, w = image_size)
+        #
 
 
 def visualization_original(generated_plot, title):
@@ -187,23 +193,22 @@ def visualization_original(generated_plot, title):
     # p.border_fill = "whitesmoke"
     p.legend.orientation = "bottom_right"
     # p.toolbar_location = "below"
-    region = generated_plot.decision_region(pca, generated_plot.all_classes[generated_plot.original])
-    p.image(image=[region], x=[min(x)], y=[min(y)], dw=[abs(max(x)-min(x))], dh=[abs(max(y)-min(y))], palette="Greys4", alpha = 0.1, name = "region")
-    plot_size = region.shape
+    # region = generated_plot.decision_region(pca, generated_plot.all_classes[generated_plot.original])
+    # p.image(image=[region], x=[min(x)], y=[min(y)], dw=[abs(max(x)-min(x))], dh=[abs(max(y)-min(y))], palette="Greys3", alpha = 0.1, name = "region")
+    # plot_size = region.shape
 
 
     for index, color, label, name, status in generated_plot.samples:
-        print len(index), len(x), label, name
         if status is "treino":
             if name is "Generated":
                 x_plot = []
                 y_plot = []
 
             else:
-                x_plot = list(x[index[generated_plot.original]])
-                y_plot = list(y[index[generated_plot.original]])
-            # p.square(x = x_plot, y = y_plot , color = color,
-            #         legend = label, size = square_size, line_dash = [6, 3], name = name)
+                print
+                print len([index[generated_plot.original]])
+                x_plot = x[index[generated_plot.original]]
+                y_plot = y[index[generated_plot.original]]
             p.scatter(x = x_plot, y = y_plot , marker = "circle", color = color,
                     legend = label, size = square_size, line_dash = [6, 3], name = name)
             # p.image_url(x = list(x[index]-image_size/2), y = list(y[index]+image_size/2), url= ['../'+df['Image path'].values[~generated][index]], global_alpha = alpha, h = image_size, w = image_size)
@@ -225,54 +230,77 @@ def visualization_all(generated_plot, title):
     # p.border_fill = "whitesmoke"
     p.legend.orientation = "bottom_right"
     # p.toolbar_location = "below"
-    region = generated_plot.decision_region(pca, generated_plot.all_classes[generated_plot.treino])
-    p.image(image=[region], x=[min(x)], y=[min(y)], dw=[abs(max(x)-min(x))], dh=[abs(max(y)-min(y))], palette="Greys4", alpha = 0.1, name = "region")
-    plot_size = region.shape
+    # region = generated_plot.decision_region(pca, generated_plot.all_classes[generated_plot.treino])
+    # p.image(image=[region], x=[min(x)], y=[min(y)], dw=[abs(max(x)-min(x))], dh=[abs(max(y)-min(y))], palette="Greys4", alpha = 0.1, name = "region")
+    # plot_size = region.shape
 
 
     for index, color, label, name, status in generated_plot.samples:
         print len(index), len(x), label, name
         if status is "treino":
             if name is "Generated":
-                x_plot = list(x[generated_plot.generated[generated_plot.treino]])
-                y_plot = list(y[generated_plot.generated[generated_plot.treino]])
+                x_plot = x[generated_plot.generated[generated_plot.treino]]
+                y_plot = y[generated_plot.generated[generated_plot.treino]]
+                images = generated_plot.images[generated_plot.generated]
 
             else:
-                x_plot = list(x[index[generated_plot.original]])
-                y_plot = list(y[index[generated_plot.original]])
-            # p.square(x = x_plot, y = y_plot , color = color,
-            #         legend = label, size = square_size, line_dash = [6, 3], name = name)
+                x_plot = x[index[generated_plot.original]]
+                y_plot = y[index[generated_plot.original]]
+                images = generated_plot.images[index]
             p.scatter(x = x_plot, y = y_plot , marker = "circle", color = color,
                     legend = label, size = square_size, line_dash = [6, 3], name = name)
-            # p.image_url(x = list(x[index]-image_size/2), y = list(y[index]+image_size/2), url= ['../'+df['Image path'].values[~generated][index]], global_alpha = alpha, h = image_size, w = image_size)
+            # p.image_url(x = list(x_plot-image_size/2), y = list(y_plot+image_size/2), url= "0.png", global_alpha = 1, h = 1000, w = 1000)
 
     return p
 
 output_server("animated")
+# output_file("animated.html")
 image_size = 300
 square_size = 10
 alpha = 0.9
 
 # features_file = sys.argv[1]
+# Plot original balanced class
+features_file = "../data/elefante-cavalo/features/BIC_Intensity_64c_100r_200i_original.csv"
+original_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
+original = visualization_original(original_plot, "Original")
+
+# Plot desbalanced class
+features_file = "../data/elefante-cavalo/Artificial/3-Rebalanced0/features/BIC_Intensity_64c_100r_200i_artificial.csv"
+unbalanced_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
+unbalanced = visualization_original(unbalanced_plot, "Desbalanceado")
+
+# Plot after images generation
 features_file = "../data/elefante-cavalo/Artificial/3-Rebalanced0/features/BIC_Intensity_64c_100r_200i_artificial.csv"
 generated_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
-p = visualization_original(generated_plot, "Original")
+generated = visualization_all(generated_plot, "Geracao Artificial de Imagens")
 
-features_file = "../data/elefante-cavalo/Artificial/3-Rebalanced0/features/BIC_Intensity_64c_100r_200i_artificial.csv"
-generated_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
-p1 = visualization_all(generated_plot, "Geracao Artificial de Imagens")
-
+# Plot after smote
 features_file = "../data/elefante-cavalo/features/BIC_Intensity_200i_smote.csv"
 smote_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
-p2 = visualization_all(smote_plot, "SMOTE")
+smote = visualization_all(smote_plot, "SMOTE")
 
-show(hplot(p, p1, p2))
+# Plot images generation testing images
+features_file = "../data/elefante-cavalo/Artificial/3-Rebalanced0/features/BIC_Intensity_64c_100r_200i_artificial.csv"
+generated_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
+generated_testing = visualization_all(generated_plot, "Geracao Artificial de Imagens")
 
-# show(p1)
-# generated_plot.animate(p1)
-# smote_plot.animate(p2)
+# Plot smote testing images
+features_file = "../data/elefante-cavalo/features/BIC_Intensity_200i_smote.csv"
+smote_plot = plot(features_file, {0: 'Elefante', 1: 'Cavalo'})
+smote_testing = visualization_all(smote_plot, "SMOTE")
 
+plot = gridplot([[original, unbalanced], [generated, smote], [generated_testing, smote_testing]], toolbar_location="above")
 
-# button = Button(label="Foo", type="success")
-# button.on_change('selected', generate)
-# show(hplot(p1, p2))
+show(plot)
+
+# tab1 = Panel(child=p, title="Scatter")
+# tab2 = Panel(child=p, title="Images")
+# tabs1 = Tabs(tabs = [tab1, tab2])
+# Animate artificial and smote generation
+# generated_data = generated_plot.animate(generated)
+# smote_data = smote_plot.animate(smote)
+# for item in range(len(generated_data)):
+#     cursession().store_objects(*generated_data[item])
+#     cursession().store_objects(*smote_data[item])
+#     time.sleep(.10)
